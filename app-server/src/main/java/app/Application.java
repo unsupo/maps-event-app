9bd99b69f0eb8079e82b320bbc6213bec6c2dda6
Jonathan Arndt <jarndt@demandware.com>
2017-10-10 14:33:11 -0600

    includes a default admin user for issue #4 and default category which closes #1

--- app-server/src/main/java/app/Application.java
+++ app-server/src/main/java/app/Application.java
@@ -1,13 +1,37 @@
 package app;
 
 import config.ConfigManager;
+import object.Category;
+import object.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import users.UserAdapter;
+
+import java.io.UnsupportedEncodingException;
+import java.security.GeneralSecurityException;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) throws Exception {
         ConfigManager.setConfigFiles(args);
+        initialize();
         SpringApplication.run(Application.class, args);
+    }
+
+    private static void initialize(){
+        //create default admin user
+        try {
+            User u = new User("admin@admin.com", "admin", "admin", "admin");
+            u.setUserGroup(User.ADMIN_GROUP);
+            UserAdapter.registerUser(u);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        //Create default category
+        try {
+            Category.addCategories("[{\"name\":\"default\",\"url\":\"assets/Petition-Icon.png\"}]");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
